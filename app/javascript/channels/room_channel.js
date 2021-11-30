c057fe2aa1122739fa49310e7b49c3a79043b536
Talha Naeem <talha.naeem@sendoso.com>
2021-11-30 13:25:27 +0500

    first commit

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,35 @@
+import { event } from "jquery";
+import consumer from "./consumer"
+
+consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    $("#msg").append('<div class="message">' + data.content + '</div>')
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
+
+var submit_messages;
+
+$(document).on('turbolinks:load', function () {
+  submit_messages()
+})
+
+
+submit_messages = function () {
+  $('#message_content').on('keydown', function (event) {
+    if (event.keyCode === 13) {
+      $('input').click()
+      event.target.value = ''
+      event.preventDefault()
+      
+    }
+  })
+}
